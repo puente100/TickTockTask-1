f4bd3c3336bcd672098b25429cea0a9e6d594ccd
DanielPuente <puente10@hotmail.com>
2024-07-01 07:43:44 -0600

    Initial Commit

---
+++ TickTockTask/TickTockTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickTockTaskApp.swift
+//  TickTockTask
+//
+//  Created by Daniel Puente on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TickTockTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
